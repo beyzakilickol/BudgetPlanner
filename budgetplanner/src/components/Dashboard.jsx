fdfe21b8491981174dfe4840cbf296ec07cec03c
beyzakilickol <43250495+beyzakilickol@users.noreply.github.com>
2019-01-30 14:08:33 -0600

    added router

---
+++ budgetplanner/src/components/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../css/output.css'
+
+class Dashboard extends Component {
+    render() {
+        return ( <div className="body">
+                hey
+                </div>
+        );
+    }
+}
+
+export default Dashboard;
